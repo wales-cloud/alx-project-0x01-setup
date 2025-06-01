cb6ff4633c7a4ea87fedf51c2a0037d216663c40
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 16:23:29 -0600

    Fix: Implement PostCard and posts page correctly

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import { PostProps } from "@/interfaces";
 
-const PostsPage: React.FC = () => {
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-6">
-        <h1 className="text-3xl font-semibold mb-4">Posts</h1>
-        <p>This is where the list of posts will go.</p>
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts?.map(({ title, body, userId, id }, key) => (
+            <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+          ))}
+        </div>
       </main>
-      <Footer />
-    </>
+    </div>
   );
 };
 
-export default PostsPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Posts;
