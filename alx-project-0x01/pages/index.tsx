faebc78e7b1962e1b5651d3a184260453c4cb0f6
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 16:15:37 -0600

    done

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,15 +1,24 @@
 import Header from "@/components/layout/Header";
 
-const PostsPage: React.FC = () => {
+const Home: React.FC = () => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-6">
-        <h1 className="text-3xl font-semibold mb-4">Posts</h1>
-        <p>This is where the list of posts will go.</p>
+      <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-white">
+            Welcome to our Application!
+          </h1>
+          <p className="mt-4 text-xl text-white">
+            We're glad you're here. Explore and enjoy your experience.
+          </p>
+          <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-gray-200 transition">
+            Get Started
+          </button>
+            </div>
       </main>
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default PostsPage;
+export default Home;
